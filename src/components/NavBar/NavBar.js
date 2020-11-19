3e6dcf57063063ed8e91c44762062a39b3caa0b8
Orlando Murcio <ato.murcio@gmail.com>
2020-11-18 20:32:08 -0800

    Toggle form

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './NavBar.css'
-export const NavBar = () => {
+export const NavBar = (props) => {
+
+
     return (
         <nav className='nav-container'>
-          <h1 className="app-title">Nasa Pik of the day</h1>
+          <h1 className="app-title">NASA Pik of the day</h1>
           <div className="link-container">
               <ul className="link-list">
                   <li className="link">About</li>
@@ -11,7 +13,12 @@ export const NavBar = () => {
                   <li className="link">Lat Month</li>
               </ul>
           </div>
-          <button className='find-button'>find</button>
+          <button 
+            onClick={props.displayForm}
+            className='find-button'
+            >
+            find
+            </button>
           
         </nav>
     )

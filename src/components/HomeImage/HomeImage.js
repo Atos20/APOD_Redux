0246de1f38b2e6b6748250f83e9ea83bfdeead58
Orlando Murcio <ato.murcio@gmail.com>
2020-11-18 20:30:48 -0800

    Display information about current image

--- src/components/HomeImage/HomeImage.js
+++ src/components/HomeImage/HomeImage.js
@@ -9,17 +9,17 @@ export const HomeImage = (props) => {
             <h1 className="picture-title">{pictureOfTheDay.title}</h1>
             <div className="date-container">
                 <h3 className="date-title">taken on</h3>
-                <p className="date">{moment(pictureOfTheDay.date).format('LLL')}</p>
+                <p className="date-title">{moment(pictureOfTheDay.date).format('LLL')}</p>
             </div>
             <img 
               src={pictureOfTheDay.url} 
               alt={pictureOfTheDay.title} 
               className="todays-image"/>
-
               <div className="description-container">
-              <h2 className="description-title">description</h2>
+                  <h2 className="description-title">description</h2>
                   <p className="description">{pictureOfTheDay.explanation}</p>
               </div>
+
         </div>
     )
 }

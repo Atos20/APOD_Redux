c583d34f6b0f4ec4f108e67715495ffd1bc95626
Orlando Murcio <ato.murcio@gmail.com>
2020-11-20 13:51:52 -0800

    Find picture by date

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPictureByDate } from '../../actions/getDataAction';
+import { updateInputField } from '../../actions/';
 import './Form.css';
 
 
-export const Form = (props) => {
+const Form = (props) => {
+
+    const submitDate = (event) => {
+        event.preventDefault()
+        console.log(props)
+        props.fetchPictureByDate(props.updateInput)
+    }
         return (
             <>
             {props.isFormVisible &&
             <form 
-                // onSubmit={(event) => submitDate(event)}
+                onSubmit={(event) => submitDate(event)}
                 className='form-container'>
               <input 
                 min='1995-06-16'
@@ -15,16 +24,21 @@ export const Form = (props) => {
                 name='date'
                 type='date' 
                 placeholder={props.today.format('YYYY-MM-DD')}
-                onChange={(event) => props.updateInput(event)}
+                onChange={(event) => props.updateInputField(event)}
                 className='date-input'/>
               <button 
+                onClick={(event) => submitDate(event)}
                 className="submit-button">
                 submit
                 </button> 
             </form>
-
             }
             </>
         )
     }
 
+const mapStateToProps = state => ({
+    updateInput:state.updateInput.date
+})
+
+export default connect(mapStateToProps, { updateInputField, fetchPictureByDate })(Form)

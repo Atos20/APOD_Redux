54754f48307026c04996100b51048b63c4d765e8
Orlando Murcio <ato.murcio@gmail.com>
2020-11-20 09:27:53 -0800

    Add try/catch statement

--- src/actions/getDataAction.js
+++ src/actions/getDataAction.js
@@ -3,11 +3,15 @@ import moment from 'moment'
 
 export const fetchPictureOfTheDay = () => async dispatch => {
     const today = moment().format('YYYY-MM-DD')
-    const pictureOfTheDay = await getPictureOfTheDay(today)
-    return dispatch({
-        type: 'FETCH_PICTURE_OF_THE_DAY',
-        payload: pictureOfTheDay
-    })
+    try{
+        const pictureOfTheDay = await getPictureOfTheDay(today)
+        return dispatch({
+            type: 'FETCH_PICTURE_OF_THE_DAY',
+            payload: pictureOfTheDay
+        })
+    } catch(error){
+        console.log(error)
+    }
  
 }
 export const fetchPreviousPictures = () => async dispatch => {
@@ -15,6 +19,11 @@ export const fetchPreviousPictures = () => async dispatch => {
     const startingDate = moment().subtract(17, 'days').format('YYYY-MM-DD');
     try{
       const lastWeekPictures = await getPicturesOfTheWeek(startingDate, today);
+     lastWeekPictures.forEach((entry, i) => {
+          entry.id = i
+          entry.isSaved = false
+          return entry
+      })
       return dispatch({
           type: 'FETCH_LAST_WEEKS_PICTURES',
           payload: lastWeekPictures
@@ -23,4 +32,11 @@ export const fetchPreviousPictures = () => async dispatch => {
       console.log(error)
     }
  
+}
+
+export const selectCurrentImage = (currentImage) => dispatch => {
+    return dispatch({
+        type: 'SELECT_CURRENT_IMAGE',
+        payload: currentImage
+    })
 }

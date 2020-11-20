89d9db104a2b87cac4fbe38529b3d037d860fbde
Orlando Murcio <ato.murcio@gmail.com>
2020-11-19 17:02:26 -0800

    Add dispatch function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
-export const toggleForm =  () => {
-    return {
+export const toggleForm =  () => dispatch => {
+    return dispatch({
         type: 'TOGGLE'
-    }
+    })
 }
 
-export const updateInput = (event) => {
-    return {
+export const updateInput = (event) => dispatch => {
+    return dispatch({
         type: 'UPDATE_INPUT',
         payload: event
-    }
+    })
 }

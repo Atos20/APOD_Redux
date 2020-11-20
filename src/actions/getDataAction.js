be2e4601466118de38150431b731499291cdd644
Orlando Murcio <ato.murcio@gmail.com>
2020-11-19 17:01:57 -0800

    Create fetch actions to get data

--- src/actions/getDataAction.js
+++ src/actions/getDataAction.js
@@ -1,14 +1,26 @@
 import { getPictureOfTheDay, getPicturesOfTheWeek } from '../apiCalls/apiCalls';
 import moment from 'moment'
 
-export const fetchPictureOfTheDay = async() => async dispatch => {
-    console.log('fetching')
+export const fetchPictureOfTheDay = () => async dispatch => {
     const today = moment().format('YYYY-MM-DD')
     const pictureOfTheDay = await getPictureOfTheDay(today)
-    console.log(pictureOfTheDay)
-    await dispatch({
+    return dispatch({
         type: 'FETCH_PICTURE_OF_THE_DAY',
         payload: pictureOfTheDay
     })
+ 
+}
+export const fetchPreviousPictures = () => async dispatch => {
+    const today = moment().format('YYYY-MM-DD');
+    const startingDate = moment().subtract(17, 'days').format('YYYY-MM-DD');
+    try{
+      const lastWeekPictures = await getPicturesOfTheWeek(startingDate, today);
+      return dispatch({
+          type: 'FETCH_LAST_WEEKS_PICTURES',
+          payload: lastWeekPictures
+      })
+    }catch(error){
+      console.log(error)
+    }
  
 }

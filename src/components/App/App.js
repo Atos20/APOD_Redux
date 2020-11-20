8d3daeca4c1a7e78b43c1ba4570662a4ef8fc277
Orlando Murcio <ato.murcio@gmail.com>
2020-11-19 17:03:02 -0800

    Migrate redux state to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,86 +1,61 @@
 import {React, Component } from 'react';
 import { HomeImage } from '../HomeImage/HomeImage';
 import { NavBar } from '../NavBar/NavBar'
-import { getPicturesOfTheWeek } from '../../apiCalls/apiCalls';
 import { PicturesContainer } from '../PicturesContainer/PicturesContainer';
 import { Form } from '../Form/Form';
-import moment from 'moment';
 import { connect } from 'react-redux';
-import { fetchPictureOfTheDay } from '../../actions/getDataAction'
+import { fetchPictureOfTheDay, fetchPreviousPictures } from '../../actions/getDataAction'
+import { updateInput, toggleForm  } from '../../actions';
+import moment from 'moment';
 import './App.css';
+
 export class App extends Component {
-  constructor(props) {
-    super(props)
-    console.log(props)
-    this.state = {
-      isLoading: true,
-      today: moment(),
-      // pictureOfTheDay: {},
-      thisWeeksPictures: {},
-      previousDays:[]
-    }
-  }
 
-  getThisWeeksPictures = async () => {
-    const today = moment();
-    const from_date = today.startOf('week').format('YYYY-MM-DD');
-    try{
-      const thisWeeksPictures = await getPicturesOfTheWeek(from_date, this.state.today.format('YYYY-MM-DD'))    
-    }catch(error){
-      console.log(error)
-    }
-  }
-
-  getPreviousImages = async () => {
-    const today = moment();
-    const startingDate = today.subtract(8, 'days').format('YYYY-MM-DD');
-    try{
-      const previousDays = await getPicturesOfTheWeek(startingDate, this.state.today.format('YYYY-MM-DD'));
-      this.setState({ previousDays }) 
-    }catch(error){
-      console.log(error)
-    }
-  }
-
-  componentDidMount = () => {
-    console.log('componentDidMount')
-    this.props.fetchPictureOfTheDay()
-    this.getThisWeeksPictures();
-    this.getPreviousImages();
+  componentDidMount = async() => {
+    await this.props.fetchPictureOfTheDay()
+    await this.props.fetchPreviousPictures();
   }
 
   render() {
-    const { 
-      pictureOfTheDay, 
-      previousDays, 
-      today, 
-      isFormVisible 
-    } = this.state
-
+  console.log(this.props)
     return (
 
         <div className="App">
-          <NavBar 
-          displayForm={this.displayForm}/>
+        
+          <NavBar
+            toggleForm={this.props.toggleForm}
+          />
 
           <Form 
-          isFormVisible={isFormVisible}
-          today={today}/>
+            isFormVisible={this.props.isFormVisible}
+            updateInput ={this.props.updateInput}
+            today={moment()}
 
+          />
           <HomeImage 
-              pictureOfTheDay={pictureOfTheDay}
+              pictureOfTheDay={this.props.pictureOfTheDay}
           />
 
           <PicturesContainer 
-              updateMainImage={this.updateMainImage}
-              previousDays={previousDays}
+              previousDays={this.props.previousPictures}
           />
 
         </div>
-
     );
   }
 
 }
+const mapStateToProps =  state => ({
+  pictureOfTheDay: state.data.pictureOfTheDay,
+  previousPictures: state.data.previousPictures,
+  isFormVisible: state.isFormVisible
+})
 
-export default connect(null, { fetchPictureOfTheDay })(App)
+export default connect(mapStateToProps, 
+  { 
+    fetchPictureOfTheDay, 
+    fetchPreviousPictures,
+    updateInput,
+    toggleForm
+  }
+  )(App)
